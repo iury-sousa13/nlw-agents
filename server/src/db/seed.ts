e27ae366bcf9bf68f7e14f7b35b8afbdddec34b1
Iury Sousa <iurysousa13@outlook.com>
2025-07-09 17:25:41 -0300

    feat: create seeds

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -2,8 +2,18 @@ import { reset, seed } from "drizzle-seed";
 import { db, sql } from "./connection";
 import { schema } from "./schema";
 
-await reset(db, { schema });
-await seed(db, { schema });
+await reset(db, schema);
+await seed(db, schema).refine((func) => {
+	return {
+		rooms: {
+			count: 20,
+			columns: {
+				name: func.companyName(),
+				description: func.loremIpsum(),
+			},
+		},
+	};
+});
 
 await sql.end();
 
